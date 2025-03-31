da99ac1bb74a03476c7c336b7ed5e8950c93e2e2
Miranda0309 <ubernemf@gmail.com>
2025-03-30 22:18:42 -0600

    se agrego las salidas input

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const peopleInput = document.querySelector(".people-input");
 const tipPerPerson = document.getElementById(".tip-amount");
 const totalPerPerson = document.getElementById("total-amount");
 const tipCustom = document.querySelector(".tip-custom");
+
+billInput.addEventListener("input", billInputFun);
+peopleInput.addEventListener("input", peopleInputFun);
+tips.forEach(function (val) {
+    val.addEventListener("click" , handleClick);
+});
+tipCustom.addEventListener("input", tipInputFun);
+
+billInput.value = "0.0";
+peopleInput.value = "1";
+tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+totalPerPerson.innerHTML = "$" + (0.0).toFixed(2);
