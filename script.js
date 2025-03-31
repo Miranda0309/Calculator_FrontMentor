f1d1edcf8b0ba7f74b3fb19183799ef63970f5ce
Miranda0309 <ubernemf@gmail.com>
2025-03-30 22:16:11 -0600

    se definieron las constantes

---
+++ script.js
@@ -0,0 +1,5 @@
+const billInput = document.querySelector(".bill-input");
+const peopleInput = document.querySelector(".people-input");
+const tipPerPerson = document.getElementById(".tip-amount");
+const totalPerPerson = document.getElementById("total-amount");
+const tipCustom = document.querySelector(".tip-custom");
